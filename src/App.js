de1c17f6b5155596733151ea179a405144701ea6
Murad Faridi <muradfaridi92@gmail.com>
2019-05-12 21:08:58 +0500

    Classes and Props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
 // import './App.css';
 
-function Cities() {
-	const cities = ['Faisalabad', 'Lahore', 'Karachi', 'Islamabad', 'Quetta', 'Peshawer', 'Faisalabad']
-	return (
-		<ul>
-			{
-				cities.map((city,index) => <li key={index}>{city}</li>)
-			}
-		</ul>
-	);
+class Cities extends React.Component {
+	render() {
+		const { pakCities, pakProvinces } = this.props
+		return (
+			<ul>
+				{
+					pakCities.map((city, index) => <li key={index}>{city}</li>)
+				}
+				<li><strong>Provinces</strong></li>
+				{
+					pakProvinces.map((city, index) => <li key={index}>{city}</li>)
+				}
+			</ul>
+		);
+	}
 }
 
-function App() {
-	return (
-		<div>
-			<Cities />
-			<Cities />
-		</div>
-	);
+class App extends React.Component {
+	render() {
+		const cities = ['Faisalabad', 'Lahore', 'Karachi', 'Islamabad', 'Quetta', 'Peshawer', 'Faisalabad']
+		const provinces = ['Punjab', 'Sindh', 'KPK', 'Balochistan', 'Capital']
+		return (
+			<div>
+				<Cities pakCities={cities} pakProvinces={provinces} />
+			</div>
+		);
+	}
 }
 
 export default App;
