42f676435b4d617d2ab00d87ccaf12fd4b1f6bba
Murad Faridi <muradfaridi92@gmail.com>
2019-05-12 17:22:07 +0500

    Data Rendering Methods

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './Test.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cities = ['Faisalabad', 'Lahore', 'Karachi', 'Islamabad', 'Quetta']
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/*
+// Method 1
+const DisplayByElement = React.createElement('ul', {
+    className : 'colorRed'
+},
+    cities.map(city => React.createElement('li', {
+        key: city
+    }, city))
+)
+ReactDOM.render(DisplayByElement, document.getElementById('root'));
+*/
+
+/*
+// Method 2
+const DisplayElementAndJSX = React.createElement('ul', {
+    className: 'colorGreen'
+},
+    cities.map(city => <h1 key={city}>{city}</h1>)
+)
+ReactDOM.render(DisplayElementAndJSX, document.getElementById('root'));
+*/
+
+// Method 3
+const DisplaybyJSX = <ul>
+                        {
+                            cities.map(city => <li key={city}>{city}</li>)
+                        }
+                    </ul> 
+ReactDOM.render(DisplaybyJSX, document.getElementById('root'));
