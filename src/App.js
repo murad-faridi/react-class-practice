9b53cabbf8e562db85d87b23c32ccb51199c7185
Murad Faridi <muradfaridi92@gmail.com>
2019-05-12 21:19:40 +0500

    Stateless Components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
 import React from 'react';
 // import './App.css';
 
-class Cities extends React.Component {
-	render() {
-		const { pakCities, pakProvinces } = this.props
-		return (
-			<ul>
-				{
-					pakCities.map((city, index) => <li key={index}>{city}</li>)
-				}
-				<li><strong>Provinces</strong></li>
-				{
-					pakProvinces.map((city, index) => <li key={index}>{city}</li>)
-				}
-			</ul>
-		);
-	}
+function Cities(props) {
+	const cities = props.pakCities
+	return (
+		<ul>
+			{
+				cities.map((city,index) => <li key={index}>{city}</li>)
+			}
+		</ul>
+	);
 }
 
-class App extends React.Component {
-	render() {
-		const cities = ['Faisalabad', 'Lahore', 'Karachi', 'Islamabad', 'Quetta', 'Peshawer', 'Faisalabad']
-		const provinces = ['Punjab', 'Sindh', 'KPK', 'Balochistan', 'Capital']
-		return (
-			<div>
-				<Cities pakCities={cities} pakProvinces={provinces} />
-			</div>
-		);
-	}
+function App() {
+	const cities = ['Faisalabad', 'Lahore', 'Karachi', 'Islamabad', 'Quetta', 'Peshawer', 'Faisalabad']
+	return (
+		<div>
+			<Cities pakCities={cities}/>
+		</div>
+	);
 }
 
 export default App;
