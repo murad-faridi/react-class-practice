57f8e72bd679b9ea939a44f588deb4c39d938c6d
Murad Faridi <muradfaridi92@gmail.com>
2019-05-13 12:17:28 +0500

    Use state in Class

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
 // import './App.css';
 
-function Cities(props) {
-	const cities = props.pakCities
-	return (
-		<ul>
-			{
-				cities.map((city,index) => <li key={index}>{city}</li>)
-			}
-		</ul>
-	);
+/* 
+*	Impotant Note:
+*	Use Class when need to use "state"
+*/
+
+class Cities extends React.Component {
+	state = {
+		cities: this.props.pakCities
+	}
+	render() {
+		return (
+			<ul>
+				{
+					this.state.cities.map((city, index) => <li key={index}>{city}</li>)
+				}
+			</ul>
+		);
+	}
 }
 
-function App() {
-	const cities = ['Faisalabad', 'Lahore', 'Karachi', 'Islamabad', 'Quetta', 'Peshawer', 'Faisalabad']
-	return (
-		<div>
-			<Cities pakCities={cities}/>
-		</div>
-	);
+class App extends React.Component {
+	render() {
+		const cities = ['Faisalabad', 'Lahore', 'Karachi', 'Islamabad', 'Quetta', 'Peshawer', 'Faisalabad']
+		return (
+			<div>
+				<Cities pakCities={cities} />
+			</div>
+		);
+	}
 }
 
 export default App;
